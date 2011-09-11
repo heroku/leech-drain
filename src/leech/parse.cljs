da3e33774bf1e24ad6ca4fd3f4b2027dbdc7f138
Mark McGranaghan <mmcgrana@gmail.com>
2011-09-11 14:17:20 -0700

    try strings

--- src/leech/parse.cljs
+++ src/leech/parse.cljs
@@ -37,7 +37,7 @@
 (defn parse-message-attrs [msg]
   (reduce
     (fn [a [_ k e v]]
-      (assoc a (keyword k)
+      (assoc a k
         (if (= "" e) true (coerce-val v))))
     {}
     (re-seq attrs-re msg)))
@@ -52,29 +52,29 @@
 (defn parse-standard-line [l]
   (if-let [m (re-matches standard-re l)]
     (merge
-      {:event_type "standard"
-       :timestamp_src (parse-timestamp (get m 1))
-       :host (get m 2)
-       :facility (get m 3)
-       :level (get m 4)
-       :component (get m 5)
-       :pid (parse-long (get m 7))
-       :slot (get m 8)
-       :ion_id (parse-long (get m 9))
-       :cloud (get m 10)})))
-      ;(parse-message-attrs (get m 11)))))
+      {"event_type" "standard"
+       "timestamp_src" (parse-timestamp (get m 1))
+       "host" (get m 2)
+       "facility" (get m 3)
+       "level" (get m 4)
+       "component" (get m 5)
+       "pid" (parse-long (get m 7))
+       "slot" (get m 8)
+       "ion_id" (parse-long (get m 9))
+       "cloud" (get m 10)}
+      (parse-message-attrs (get m 11)))))
 
 (def raw-re
   #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) (.*)$")
 
 (defn parse-raw-line [l]
   (if-let [m (re-matches raw-re l)]
-    {:event_type "raw"
-     :timestamp_src (parse-timestamp (get m 1))
-     :host (get m 2)
-     :facility (get m 3)
-     :level (get m 4)
-     :message (get m 5)}))
+    {"event_type" "raw"
+     "timestamp_src" (parse-timestamp (get m 1))
+     "host" (get m 2)
+     "facility" (get m 3)
+     "level" (get m 4)
+     "message" (get m 5)}))
 
 (def nginx-access-re
      ;timestamp_src                              ;host      ;facility    ;level           ;slot        ;ion_id ;cloud           ;http_host                                                              ;http_method,_url,_version      ;http_status,_bytes,_referrer,_user_agent,_domain
@@ -82,57 +82,57 @@
 
 (defn parse-nginx-access-line [l]
   (if-let [m (re-matches nginx-access-re l)]
-    {:event_type "nginx_access"
-     :timestamp_src (parse-timestamp (get m 1))
-     :host (get m 2)
-     :facility (get m 3)
-     :level (get m 4)
-     :component "nginx"
-     :slot (get m 5)
-     :ion_id (parse-long (get m 6))
-     :cloud (get m 7)
-     :http_host (get m 8)
-     :http_method (get m 9)
-     :http_url (get m 10)
-     :http_version (get m 11)
-     :http_status (parse-long (get m 12))
-     :http_bytes (parse-long (get m 13))
-     :http_referrer (get m 14)
-     :http_user_agent (get m 15)
-     :http_domain (get m 16)}))
+    {"event_type" "nginx_access"
+     "timestamp_src" (parse-timestamp (get m 1))
+     "host" (get m 2)
+     "facility" (get m 3)
+     "level" (get m 4)
+     "component" "nginx"
+     "slot" (get m 5)
+     "ion_id" (parse-long (get m 6))
+     "cloud" (get m 7)
+     "http_host" (get m 8)
+     "http_method" (get m 9)
+     "http_url" (get m 10)
+     "http_version" (get m 11)
+     "http_status" (parse-long (get m 12))
+     "http_bytes" (parse-long (get m 13))
+     "http_referrer" (get m 14)
+     "http_user_agent" (get m 15)
+     "http_domain" (get m 16)}))
 
 (def nginx-error-re
   #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d-\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) nginx - ([a-z4-6]+)?\.(\d+)@([a-z.]+\.com) - .* \[error\] (.*)$")
 
 (defn parse-nginx-error-line [l]
   (if-let [m (re-matches nginx-error-re l)]
-    {:event_type "nginx_error"
-     :timestamp_src (parse-timestamp (get m 1))
-     :host (get m 2)
-     :facility (get m 3)
-     :level (get m 4)
-     :component "nginx"
-     :slot (get m 5)
-     :ion_id (parse-long (get m 6))
-     :cloud (get m 7)
-     :message (get m 8)}))
+    {"event_type" "nginx_error"
+     "timestamp_src" (parse-timestamp (get m 1))
+     "host" (get m 2)
+     "facility" (get m 3)
+     "level" (get m 4)
+     "component" "nginx"
+     "slot" (get m 5)
+     "ion_id" (parse-long (get m 6))
+     "cloud" (get m 7)
+     "message" (get m 8)}))
 
 (def varnish-access-re
   #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) varnish\[(\d+)\] - ([a-z4-6\-]+)?\.(\d+)@([a-z.]+\.com) - [0-9\.]+ - - .*\" (\d\d\d) .*$")
 
 (defn parse-varnish-access-line [l]
   (if-let [m (re-matches varnish-access-re l)]
-    {:event_type "varnish_access"
-     :timestamp_src (parse-timestamp (get m 1))
-     :host (get m 2)
-     :facility (get m 3)
-     :level (get m 4)
-     :component "varnish"
-     :pid (parse-long (get m 5))
-     :slot (get m 6)
-     :ion_id (parse-long (get m 7))
-     :cloud (get m 8)
-     :http_status (parse-long (get m 9))}))
+    {"event_type" "varnish_access"
+     "timestamp_src" (parse-timestamp (get m 1))
+     "host" (get m 2)
+     "facility" (get m 3)
+     "level" (get m 4)
+     "component" "varnish"
+     "pid" (parse-long (get m 5))
+     "slot" (get m 6)
+     "ion_id" (parse-long (get m 7))
+     "cloud" (get m 8)
+     "http_status" (parse-long (get m 9))}))
 
 (defn log [data]
   (util/log (merge {:ns "parse"} data)))
